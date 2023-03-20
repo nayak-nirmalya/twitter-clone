7679bfef351aaba365eae7ebc07c9e5dab774d22
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-20 20:43:48 +0530

    added sidebar logo twitter

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,13 +8,13 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
-      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+      <div className="container h-full mx-auto lg:mx-12 xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
           <div
             className="
                     col-span-3
-                    lg:col-span-2.4
+                    lg:col-span-2.2
                     border-x-[1px]
                     border-neutral-800
                     
--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profiel",
+      href: "/users/7008",
+      icon: FaUser,
+    },
+  ];
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
