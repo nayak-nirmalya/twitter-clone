0cb84ad15966950cd0516daa1d9960bbbbc7d4da
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-20 20:24:05 +0530

    added new layout  comp

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <div
+            className="
+                    col-span-3
+                    lg:col-span-2.4
+                    border-x-[1px]
+                    border-neutral-800
+                    
+                "
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
