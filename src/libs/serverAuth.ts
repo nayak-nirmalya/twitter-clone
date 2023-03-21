04a7fda527d9ba52fba4edc548712446f8b501f9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-21 13:06:59 +0530

    added func to authenticate

---
+++ src/libs/serverAuth.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+
+import prisma from "@/libs/prismadb";
+
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({ req });
+
+  if (!session?.user?.email) throw new Error("Not Signed In!");
+
+  const currentUser = await prisma?.user.findUnique({
+    where: {
+      email: session.user.email
+    }
+  });
+
+  if (!currentUser)
+    throw new Error(`No User With E-Mail: ${session.user.email}`);
+
+  return currentUser;
+};
+
+export default serverAuth;
