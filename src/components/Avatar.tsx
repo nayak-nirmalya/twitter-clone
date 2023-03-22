6c5bf95b8f4df09855845146a6931a9a0a0081b8
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-22 20:37:14 +0530

    added avatar comp

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface AvatarProps {
+  userId: string;
+  isLarge?: boolean;
+  hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
+  return <div>Avatar</div>;
+};
+
+export default Avatar;
