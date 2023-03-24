e314897e8a77c5e4a3484bd483ed29c3c1cd621d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-24 17:45:59 +0530

    added api to update user

---
+++ src/pages/api/edit.ts
@@ -0,0 +1,37 @@
+import serverAuth from "@/libs/serverAuth";
+import prisma from "@/libs/prismadb";
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "PATCH") return res.status(405).end();
+
+  try {
+    const { currentUser } = await serverAuth(req);
+
+    const { name, username, bio, profileImage, coverImage } = req.body;
+
+    if (!name || !username) throw new Error("Missing Required Fields!");
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id
+      },
+      data: {
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage
+      }
+    });
+
+    return res.status(200).send(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(400).end();
+  }
+}
