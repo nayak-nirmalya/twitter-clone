704ef32c6272b6753b23771a4ee35495da650ea5
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-25 15:55:22 +0530

    show user specific posts on profile page

--- src/pages/users/[userId].tsx
+++ src/pages/users/[userId].tsx
@@ -7,6 +7,7 @@ import React from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
+import PostFeed from "@/components/posts/PostFeed";
 
 const UserView = () => {
   const router = useRouter();
@@ -41,6 +42,7 @@ const UserView = () => {
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </>
   );
 };
