819a9d5de075fba6388a88c017c8e8e13f088eec
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-28 18:03:20 +0530

    redirect to home if no session in notificaions

---
+++ src/pages/notifications.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Head from "next/head";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+import Header from "@/components/Header";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {
+      session
+    }
+  };
+}
+
+const Notifications = () => {
+  return (
+    <>
+      <Head>
+        <title>Notifications - Twitter</title>
+        <meta name="description" content="Twitter Notifications Page." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/twitter_icon.ico" />
+      </Head>
+      <Header label="Notifications" showBackArrow />
+    </>
+  );
+};
+
+export default Notifications;
