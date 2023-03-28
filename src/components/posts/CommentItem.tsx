dd51fbd1a7da3587910ec7afa9e27a8c5ee80419
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-28 17:30:26 +0530

    added styling to comment item

--- src/components/posts/CommentItem.tsx
+++ src/components/posts/CommentItem.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import React, { useCallback, useMemo } from "react";
 import { formatDistanceToNowStrict } from "date-fns";
+import Avatar from "../Avatar";
 
 interface CommentItemProps {
   comment: Record<string, any>;
@@ -24,7 +25,45 @@ const CommentItem: React.FC<CommentItemProps> = ({ comment }) => {
     return formatDistanceToNowStrict(new Date(comment?.createdAt));
   }, [comment?.createdAt]);
 
-  return <div>{comment.id}</div>;
+  return (
+    <div
+      className="
+            border-p-[1px]
+            cursor-pointer
+            border-neutral-800
+            p-5
+            transition
+            hover:bg-neutral-900
+        "
+    >
+      <div className="flex flex-row items-start gap-3">
+        <Avatar userId={comment.user.id} />
+        <div>
+          <div className="flex flex-row items-center gap-2">
+            <p
+              onClick={goToUser}
+              className="cursor-pointer font-semibold text-white hover:underline"
+            >
+              {comment.user.name}
+            </p>
+            <span
+              className="
+                    hidden 
+                    cursor-pointer 
+                    text-neutral-500 
+                    hover:underline 
+                    md:block
+                "
+            >
+              @{comment.user.username}
+            </span>
+            <span className="text-sm text-neutral-500">{createdAt}</span>
+          </div>
+          <div className="mt-1 text-white">{comment.body}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CommentItem;
